00045480c5800faaf383e35817a40c3dd3b22d27
Sonu <notifications3972@gmail.com>
2018-01-24 13:04:04 -0800

    Initial commit

---
+++ java/tech/gllc/epicbattery/FirstFragment.java
@@ -0,0 +1,28 @@
+package tech.gllc.epicbattery;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+/**
+ * Created by notif on 1/19/2018.
+ */
+
+public class FirstFragment extends Fragment {
+
+
+    View view;
+    Button firstButton;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+// Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_first, container, false);
+        return view;
+    }
+}
